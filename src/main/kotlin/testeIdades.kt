2431d080ea45b934f7dff6468bad6b36673b5a8f
AmneSampaio <amnesf@gmail.com>
2021-06-13 18:54:38 -0300

    Adding forEach for 'menorIdade', comments and Int.MIN_VALUE Int.MAX_VALUE

--- src/main/kotlin/testeIdades.kt
+++ src/main/kotlin/testeIdades.kt
@@ -3,10 +3,17 @@ import kotlin.IntArray
 fun main() {
     val idades: IntArray = intArrayOf(25, 30, 21, 17)
 
-    var maiorIdade = 0
-    for (idade in idades) {
+    var maiorIdade = Int.MIN_VALUE // garante que qualquer valor inteiro vai ser maior que maiorIdade
+    for (idade in idades) { // estrutura da linguagem, loop
         if (idade > maiorIdade) {
             maiorIdade = idade
         }
     }
+    println(maiorIdade)
+
+    var menorIdade = Int.MAX_VALUE // garante que qualquer valor inteiro vai ser menor que menorIdade
+    idades.forEach { idade -> if (idade < menorIdade) { menorIdade = idade }} // forEach é uma função pronta,
+                                                                        // recebe um parâmetro e executa um código
+
+    println(menorIdade)
 }
