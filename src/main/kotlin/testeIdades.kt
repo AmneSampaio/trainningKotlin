d92cb182670356933075596c3052df9be22001c1
AmneSampaio <amnesf@gmail.com>
2021-06-15 12:28:03 -0300

    Testando funções nativas ( max, min, average)

--- src/main/kotlin/testeIdades.kt
+++ src/main/kotlin/testeIdades.kt
@@ -10,10 +10,28 @@ fun main() {
         }
     }
     println(maiorIdade)
+    // testes simples
+
+    val letras = 'a'..'z'
+    val umaSo = 'w'
+    val maxLetras = letras.max()
+    val minLetras = letras.min()
+    println(" $maxLetras $minLetras ")
+    println(umaSo in letras)
+
+    val max = idades.max()
+    val min = idades.min()
+    val average = idades.average()
+    val deMaior = idades.all { it > 18 }
+
+    println(" Máximo: $max \n Mínimo: $min\n Média: $average\n São todos maiores de idade? $deMaior \n")
 
     var menorIdade = Int.MAX_VALUE // garante que qualquer valor inteiro vai ser menor que menorIdade
     idades.forEach { idade -> if (idade < menorIdade) { menorIdade = idade }} // forEach é uma função pronta,
                                                                         // recebe um parâmetro e executa um código
 
     println(menorIdade)
+
+
+
 }
